3cacffb7c30e25d9e032e08e31ab43c3e9094de4
Orange-potato <55128153+Orange-potato@users.noreply.github.com>
2021-12-16 15:12:46 +0900

    2nd

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+// Project name: Bridgeless Totem-Pole Power Factor Correction
+// Target module: TMS320S28377D
+// Covered by JunSeong
+
+#include "F28x_Project.h"   // 칩-지원 헤더파일 및 기타 헤더파일들 일괄삽입
+#include "setting.h"    // 칩-지원 헤더파일 및 기타 헤더파일들 일괄삽입
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <math.h>
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_can.h"
+#include "driverlib/can.h"
+
+/**
+ * hello.c
+ */
+int main(void)
+{
+	printf("Hello World!\n");
+    printf("Hello World!\n");
+    printf("Hello World!\n");
+	
+	return 0;
+}
